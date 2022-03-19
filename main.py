248437269d22209a46312093aabcde6dd7d1e70b
Kreoniz <Kreoniz@mail.ru>
2022-03-19 14:02:37 +0600

    astronaut selection

--- main.py
+++ main.py
@@ -65,5 +65,71 @@ def promotion_image():
                   </body>
                 </html>"""
 
+@app.route('/astronaut_selection')
+def astronaut_selection():
+    return f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+    <title>Анкета</title>
+    <link rel="stylesheet" href="{url_for('static', filename='css/style.css')}">
+</head>
+<body>
+    <div class="container">
+        <h1 class="application">Анкета отбора кандидатов</h1>
+        <p class="application">Для участия в миссии</p>
+        <form action="#" method="post" enctype="multipart/form-data">
+            <div>
+                <p class="input-text"><input placeholder="Введите фамилию" type="text" name="surname"></p>
+                <p class="input-text"><input placeholder="Введите имя" type="text" name="name"></p>
+                <p class="input-email"><input placeholder="Введите адрес почты" type="text" name="email"></p>
+            </div>
+            <div>
+                <p class="text">Какое у вас образование?</p>
+                <select name="education">
+                    <option value="high">Высшее</option>
+                    <option value="primary">Среднее</option>
+                    <option value="banana">Я банан</option>
+                </select>
+            </div>
+            <div class="profession">
+                <p class="text">Выберите Вашу профессию:</p>
+                <p><input class="input-checkbox" type="checkbox" name="engineer-explorer">инженер-исследователь</p>
+                <p><input class="input-checkbox" type="checkbox" name="pilot">пилот</p>
+                <p><input class="input-checkbox" type="checkbox" name="builder">строитель</p>
+                <p><input class="input-checkbox" type="checkbox" name="exobiologist">экзобиолог</p>
+                <p><input class="input-checkbox" type="checkbox" name="doctor">врач</p>
+                <p><input class="input-checkbox" type="checkbox" name="terraformation-engineer">инженер по терраформированию</p>
+                <p><input class="input-checkbox" type="checkbox" name="climatologist">климатолог</p>
+                <p><input class="input-checkbox" type="checkbox" name="astrogeologist">астрогеолог</p>
+                <p><input class="input-checkbox" type="checkbox" name="glaciologist">гляциолог</p>
+                <p><input checked class="input-checkbox" type="checkbox" name="programmer">программист</p>
+                <p><input class="input-checkbox" type="checkbox" name="">...</p>
+            </div>
+            <div class="sex">
+                <p class="text">Укажите пол</p>
+                <p><input class="input-radio" type="radio" name="sex" value="male">Мужской</p>
+                <p><input class="input-radio" type="radio" name="sex" value="female">Женский</p>
+            </div>
+            <div class="additional-info">
+                <p class="text">Почему Вы хотите принимаете участие в миссии?</p>
+                <textarea name="info" placeholder="Y?"></textarea>
+            </div>
+            <div class="photo">
+                <p class="text">Пришлите нам свою фотографию</p>
+                <input type="file" name="photo">
+            </div>
+            <p><input class="input-checkbox" type="checkbox" name="live_on_mars">Готовы ли вы остаться на Марсе?</p>
+            <div>
+                <button type="submit">Принять</button>
+                <button type="reset">Сбросить</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
