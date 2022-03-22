9d78bcbef4848790e7ded2a905c2b13c2c7a1b11
Kreoniz <Kreoniz@mail.ru>
2022-03-22 14:42:29 +0600

    space training;

--- main.py
+++ main.py
@@ -8,10 +8,25 @@ app = Flask(__name__)
 @app.route('/index/<name>')
 def index_dynamic_title(name):
     params = {}
-    print(name)
     params['title'] = name
     params['css_dest'] = url_for('static', filename='css/style.css')
     return render_template('base.html', **params)
+
+@app.route('/training/<prof>')
+def training(prof):
+    params = {}
+    params['title'] = 'Расписание тренировок'
+    params['css_dest'] = url_for('static', filename='css/style.css')
+    params['prof'] = prof
+    if 'инженер' in prof.lower() or 'строитель' in prof.lower():
+        params['text'] = 'Инженерные тренажеры'
+        params['pic_dest'] = url_for('static', filename='img/spacecraft2.jpg')
+    else:
+        params['text'] = 'Научные симуляторы'
+        params['pic_dest'] = url_for('static', filename='img/spacecraft1.jpg')
+    
+    return render_template('training.html', **params)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
